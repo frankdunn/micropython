777132b71b01693667e938cba51a22b1fb5c2664
Frank Dunn <frankdunn37@gmail.com>
2017-11-23 21:17:09 +0200

    testing with 1 gpio

--- modbus_tcp_server/main.py
+++ modbus_tcp_server/main.py
@@ -1,15 +1,19 @@
 
 import modfunctions as m
-from machine import Timer
-#timer_disable = False
-timer_disable = True
+import machine
+from  machine import Timer
+
 tim = Timer(-1)
+p0 = machine.Pin(2, machine.Pin.IN)     # create input pin on GPIO2
+
 
 def func(t):
     m.holdings[8] = m.holdings[8] + 1
     print ("timer_func")
+    print(p0.value())       # get value, 0 or 1
+    m.holdings[5] = p0.value()
     
-tim.init(period=1000, mode=Timer.PERIODIC, callback=func)
+tim.init(period=100, mode=Timer.PERIODIC, callback=func)
 
 m.mod_main()
 
